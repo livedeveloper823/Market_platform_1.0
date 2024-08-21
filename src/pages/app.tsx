b01e58b682da7d40bb849796ec2e9da758244e1f
laihaoqiang <laihaoqiang34@gmail.com>
2024-08-21 13:48:37 +1000

    image

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Button from "../components/Button/Button";
 import { TrendingUp } from "lucide-react";
-import SemiCircularProgressBar from "../components/feature/Circlar";
 
 import TopEventCard from "../components/cards/TopEventCard";
 import TopNavbar from "../components/TopNavbar";
